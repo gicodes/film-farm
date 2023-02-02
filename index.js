d3aa78913582ceef2616657b3637f6c86f6e5b8e
Gideon, I <104095690+gicodes@users.noreply.github.com>
2023-02-02 11:26:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const autoCompleteConfig = {
   async fetchData(searchTerm) {
     const response = await axios.get('http://www.omdbapi.com/', {
       params: {
-        apikey: 'd9835cc5',
+        apikey: 'f93e1c99',
         s: searchTerm
       }
     });
